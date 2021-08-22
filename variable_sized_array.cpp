82030ac80192a723c97c48aba20640dd48ad5eb9
sheryl-001 <89308454+sheryl-001@users.noreply.github.com>
2021-08-22 19:43:00 +0530

    Add files via upload

---
+++ variable_sized_array.cpp
@@ -0,0 +1,20 @@
+//You will be given an array of N integers and you have to print the integers in the reverse order.
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    for(i=n-1;i>=0;i--)
+        cout<<a[i]<<"\t";
+    return 0;
+}
+
